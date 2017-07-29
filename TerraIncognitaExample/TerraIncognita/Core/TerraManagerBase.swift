506609688732d4c67efaccc9c289578ee80ff3aa
Valeriy Bezuglyy <valnocorner@gmail.com>
2017-07-29 22:31:17 +0300

    added terra manager

---
+++ TerraIncognitaExample/TerraIncognita/Core/TerraManagerBase.swift
@@ -0,0 +1,17 @@
+//
+//  TerraManagerBase.swift
+//  TerraIncognitaExample
+//
+//  Created by Valeriy Bezuglyy on 29/07/2017.
+//  Copyright © 2017 Valeriy Bezuglyy. All rights reserved.
+//
+
+import Foundation
+
+class TerraManagerBase: TerraManager {
+    weak var delegate:TerraManagerDelegate?
+    
+    init(config:TerraManagerConfig) {
+        
+    }
+}
