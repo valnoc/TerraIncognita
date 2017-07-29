0c393d364db01bc9d872938df1d3f3eacf1c5dfc
Valeriy Bezuglyy <valnocorner@gmail.com>
2017-07-29 20:21:37 +0300

    added empty example project

---
+++ TerraIncognitaExample/TerraIncognitaExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TerraIncognitaExample
+//
+//  Created by Valeriy Bezuglyy on 29/07/2017.
+//  Copyright © 2017 Valeriy Bezuglyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
