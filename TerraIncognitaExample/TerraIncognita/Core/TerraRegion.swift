8b1a97cb635393f286490e819ab9ae69af982de7
Valeriy Bezuglyy <valnocorner@gmail.com>
2017-07-30 13:44:14 +0300

    region contains coorindate check

--- TerraIncognitaExample/TerraIncognita/Core/TerraRegion.swift
+++ TerraIncognitaExample/TerraIncognita/Core/TerraRegion.swift
@@ -15,31 +15,39 @@ class TerraRegion {
     var deltaLat: CLLocationDegrees
     var deltaLon: CLLocationDegrees
     
+    var northWest: CLLocationCoordinate2D
+    var southEast: CLLocationCoordinate2D
+    
     init(center: CLLocationCoordinate2D,
          deltaLat: CLLocationDegrees,
          deltaLon: CLLocationDegrees) {
         self.center = center
         self.deltaLat = deltaLat
         self.deltaLon = deltaLon
+        
+        self.northWest = CLLocationCoordinate2D(latitude: center.latitude - deltaLat,
+                                                longitude: center.longitude + deltaLon)
+        self.southEast = CLLocationCoordinate2D(latitude: center.latitude + deltaLat,
+                                                longitude: center.longitude - deltaLon)
     }
     
     func containsCoordinate(_ coordinate:CLLocationCoordinate2D) -> Bool {
-        //TODO: implement check
-        return false
-    }    
-
-//    //MARK: northWest & southEast
-//    func makeNorthWestSouthEast() -> (CLLocationCoordinate2D, CLLocationCoordinate2D) {
-//        return (makeNorthWest(), makeSouthEast())
-//    }
-//    
-//    func makeNorthWest() -> CLLocationCoordinate2D {
-//        return CLLocationCoordinate2D(latitude: center.latitude - deltaLat,
-//                                      longitude: center.longitude + deltaLon)
-//    }
-//    
-//    func makeSouthEast() -> CLLocationCoordinate2D {
-//        return CLLocationCoordinate2D(latitude: center.latitude + deltaLat,
-//                                      longitude: center.longitude - deltaLon)
-//    }
+        return coordinate.latitude >= northWest.latitude && coordinate.latitude <= southEast.latitude &&
+            coordinate.longitude >= southEast.longitude && coordinate.longitude <= northWest.longitude
+    }
+    
+    //MARK: northWest & southEast
+    fileprivate func makeNorthWestSouthEast() -> (CLLocationCoordinate2D, CLLocationCoordinate2D) {
+        return (makeNorthWest(), makeSouthEast())
+    }
+    
+    fileprivate func makeNorthWest() -> CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: center.latitude - deltaLat,
+                                      longitude: center.longitude + deltaLon)
+    }
+    
+    fileprivate func makeSouthEast() -> CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: center.latitude + deltaLat,
+                                      longitude: center.longitude - deltaLon)
+    }
 }
