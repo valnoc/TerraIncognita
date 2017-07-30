570c80995235678bd5087577c69fe377bb5d5489
Valeriy Bezuglyy <valnocorner@gmail.com>
2017-07-31 00:10:25 +0300

    disable region filtering

--- TerraIncognita/Core/Manager/TerraManagerBase.swift
+++ TerraIncognita/Core/Manager/TerraManagerBase.swift
@@ -53,8 +53,9 @@ class TerraManagerBase: TerraManager {
             guard let __self = self else { return }
             guard let terraView = __self.terraView else { return }
             
-            let region = terraView.currentRegion()
-            let markersInside = markers.filter({ region.containsCoordinate($0.coordinate) })
+//            let region = terraView.currentRegion()
+//            let markersInside = markers.filter({ region.containsCoordinate($0.coordinate) })
+            let markersInside = markers
             
             __self.showActualViewMarkersOnTerraView(markersInside)
         }
