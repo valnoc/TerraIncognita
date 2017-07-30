a786190d4a24e2013b051299346ec3e3d770b2f3
Valeriy Bezuglyy <valnocorner@gmail.com>
2017-07-30 17:06:45 +0300

    reuse viewMarker only if reuseIdenfier != nil

--- TerraIncognita/Core/View/TerraViewObjectsPool.swift
+++ TerraIncognita/Core/View/TerraViewObjectsPool.swift
@@ -11,8 +11,8 @@ import Foundation
 class TerraViewObjectsPool {
     
     let maxPoolCount = 5
-    
     var freeViewMarkers: [TerraViewMarker]
+    
     var usedViewMarkers: [TerraViewMarker]
     
     init() {
@@ -22,25 +22,33 @@ class TerraViewObjectsPool {
     
     //MARK: dequeue / enqueue
     func dequeueReusableViewMarker(_ markerId: String, reuseIdentifier:String?) -> TerraViewMarker {
-        if let usedMarker = usedViewMarkers.filter({ $0.terra_markerId == markerId && $0.terra_reuseIdentifier == reuseIdentifier}).first {
-            return usedMarker
+        var marker: TerraViewMarker? = nil
+        
+        if let reuseIdentifier = reuseIdentifier {
+            if let usedMarker = usedViewMarkers.filter({ $0.terra_markerId == markerId && $0.terra_reuseIdentifier == reuseIdentifier}).first {
+                marker = usedMarker
+            }
+            else if var freeMarker = freeViewMarkers.first {
+                freeMarker.terra_markerId = markerId
+                freeMarker.terra_reuseIdentifier = reuseIdentifier
+                marker = freeMarker
+            }
         }
-        else if var freeMarker = freeViewMarkers.first {
-            freeMarker.terra_markerId = markerId
-            freeMarker.terra_reuseIdentifier = reuseIdentifier
-            return freeMarker
+        
+        if marker == nil {
+            marker = makeViewMarker(markerId, reuseIdentifier: reuseIdentifier)
         }
-        else {
-            let newFreeMarker = makeViewMarker(markerId, reuseIdentifier: reuseIdentifier)
-            usedViewMarkers.append(newFreeMarker)
-            return newFreeMarker
-        }
+        
+        usedViewMarkers.append(marker!)
+        return marker!
     }
     
     func enqueueReusableViewMarker(_ viewMarker:TerraViewMarker) {
         let markerId = viewMarker.terra_markerId
         guard let index = usedViewMarkers.index(where: {$0.terra_markerId == markerId}) else { return }
         usedViewMarkers.remove(at: index)
+        
+        guard viewMarker.terra_reuseIdentifier != nil else { return }
         if freeViewMarkers.count < maxPoolCount {
             freeViewMarkers.append(viewMarker)
         }
