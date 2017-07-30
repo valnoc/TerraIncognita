8da3affc708ba7ed1847916f5a7d59496cd3823d
Valeriy Bezuglyy <valnocorner@gmail.com>
2017-07-30 16:04:30 +0300

    added TI factory

---
+++ TerraIncognita/TerraIncogintaFactory.swift
@@ -0,0 +1,24 @@
+//
+//  TerraIncogintaFactory.swift
+//  TerraIncognitaExample
+//
+//  Created by Valeriy Bezuglyy on 30/07/2017.
+//  Copyright © 2017 Valeriy Bezuglyy. All rights reserved.
+//
+
+import Foundation
+
+enum TerraIncogintaSource {
+    case AMaps
+}
+
+class TerraIncogintaFactory {
+
+    func makeTerraManager(source:TerraIncogintaSource, config:TerraManagerConfig) -> TerraManager {
+        switch source {
+        case .AMaps:
+            return AMapsManagerObj(config: config as! AMapsManagerConfig)
+        }
+    }
+    
+}
