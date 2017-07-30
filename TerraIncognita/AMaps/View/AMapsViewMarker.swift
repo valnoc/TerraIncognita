1d818b7440e9e2e22c3632937aea8abc15dbc924
Valeriy Bezuglyy <valnocorner@gmail.com>
2017-07-30 23:31:44 +0300

    changed amaps marker to be mk annotation

--- TerraIncognita/AMaps/View/AMapsViewMarker.swift
+++ TerraIncognita/AMaps/View/AMapsViewMarker.swift
@@ -9,7 +9,16 @@
 import Foundation
 import MapKit
 
-class AMapsViewMarker: MKAnnotationView, TerraViewMarker {
+class AMapsViewMarker: NSObject, TerraViewMarker {
     var terra_markerId: String!
     var terra_reuseIdentifier: String?
+    var terra_coordinate: CLLocationCoordinate2D!
+}
+
+extension AMapsViewMarker: MKAnnotation {
+    public var coordinate: CLLocationCoordinate2D {
+        get{
+            return terra_coordinate
+        }
+    }
 }
